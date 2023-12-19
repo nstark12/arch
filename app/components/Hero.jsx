6899199e6a1ade1b16cceb542bcf04c6da7a0f50
Marketing Director <nicolestark1103@gmail.com>
2023-12-19 10:04:44 -0600

    hamburger icon added hid everything esle

---
+++ app/components/Hero.jsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,19 +1,26 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <div>
-      <header className="flex sm:flex-row justify-between items-center flex-col">
-        <Link href={"/"}>
-          <Image
-            src={"/image/logo.png"}
-            alt="ARCh logo"
-            width={350}
-            height={350}
-          />
-        </Link>
-        <div className="m-4">
+      <header className="flex flex-row justify-between items-center">
+        <div className="flex">
+          <Link href={"/"}>
+            <Image
+              src={"/image/logo.png"}
+              alt="ARCh logo"
+              width={350}
+              height={350}
+            />
+          </Link>
+          <Bars3Icon className="h-10 w-10 text-white bg-primary border border-primary rounded-lg m-6 md:hidden absolute right-0 top-1" />
+        </div>
+        <div className="m-4 hidden md:flex">
           <button className="bg-secondary text-white py-2 px-4 rounded-full mr-4 md:text-lg hover:bg-secondaryHov cursor-pointer">
             Contact Us
           </button>
@@ -22,7 +29,7 @@ const Navbar = () => {
           </button>
         </div>
       </header>
-      <nav className="bg-primary text-white py-2">
+      <nav className="bg-primary text-white py-2 md:block hidden">
         <ul className="flex justify-center md:gap-10 lg:gap-24 xl:gap-32">
           <Link className="hover:text-gray-300" href={"/"}>
             Resources
